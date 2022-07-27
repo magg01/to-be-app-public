8aefcd71ce5c305e6d2daceb3e79da833c63ede6
Matthew <Magg01@gmail.com>
2022-07-27 15:08:42 +0200

    added title of to be to add plan component

--- modules/components/addPlan.js
+++ modules/components/addPlan.js
@@ -1,10 +1,17 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { StyleSheet, View, Text, TextInput, Alert, TouchableOpacity } from 'react-native'
 import * as db from '../database/database';
 
 export default AddPlan = (props) => {
   const toBeId = useRef(props.toBeId);
   const [newPlanTitle, setNewPlanTitle] = useState('');
+  const [toBeItem, setToBeItem] = useState(undefined);
+
+  useEffect(() => {
+    db.getToBeItemById(toBeId.current).then((toBeItem) => {
+      setToBeItem(toBeItem);
+    })
+  }, [toBeId.current])
 
   const addPlan = () => {
     db.addPlan(newPlanTitle, toBeId.current)
@@ -25,21 +32,29 @@ export default AddPlan = (props) => {
     Alert.alert("Add to calendar here");
   }
 
-  return (
-    <View style={styles.container}>
-      <Text>How can you be </Text>
-      <TextInput style={styles.input} onChangeText={(text) => setNewPlanTitle(text)} />
-      <TouchableOpacity style={styles.addButton} onPress={addPlan}>
-        <Text>Add</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.addButton} onPress={addNotification}>
-        <Text>Not</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.addButton} onPress={addCalendar}>
-        <Text>Cal</Text>
-      </TouchableOpacity>
-    </View>
-  )
+  if(toBeItem === undefined){
+    return(
+      <View style={styles.container}>
+        <Text>Loading from database</Text>
+      </View>  
+    )
+  } else {
+    return (
+      <View style={styles.container}>
+        <Text style={{color: 'white', fontSize: 20}}>How can you be more {toBeItem.title.toLowerCase()}?</Text>
+        <TextInput style={styles.input} onChangeText={(text) => setNewPlanTitle(text)} />
+        <TouchableOpacity style={styles.addButton} onPress={addPlan}>
+          <Text>Add</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.addButton} onPress={addNotification}>
+          <Text>Not</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.addButton} onPress={addCalendar}>
+          <Text>Cal</Text>
+        </TouchableOpacity>
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
