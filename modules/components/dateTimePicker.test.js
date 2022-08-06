43c8bc93e87c01a852e9b106e95640da50e2b54e
Matthew <Magg01@gmail.com>
2022-08-06 23:57:05 +0200

    simplest test imaginable running on DateTimePicker

---
+++ modules/components/dateTimePicker.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import DateTimePicker from '../components/dateTimePicker';
+import { render } from '@testing-library/react-native'
+
+it("renders", () => {
+  render(<DateTimePicker />);
+})
