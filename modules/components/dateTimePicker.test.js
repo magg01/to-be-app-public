e8ccf87dd951b00f9942bd033c312506e375c568
Matthew <Magg01@gmail.com>
2022-08-10 00:00:24 +0200

    deleted test file

--- modules/components/dateTimePicker.test.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import DateTimePicker from '../components/dateTimePicker';
-import { render } from '@testing-library/react-native'
-
-it("renders", () => {
-  render(<DateTimePicker />);
-})
