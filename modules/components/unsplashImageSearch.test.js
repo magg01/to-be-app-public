954a45cb130c7466a9ed0088abd7cb7aea2f1a19
Matthew <Magg01@gmail.com>
2022-08-11 21:03:35 +0200

    added tests

--- modules/components/unsplashImageSearch.test.js
+++ modules/components/unsplashImageSearch.test.js
@@ -1,11 +1,11 @@
 // import React from 'react';
 import { render, screen, cleanup, waitFor, act, fireEvent} from '@testing-library/react-native';
 import UnsplashImageSearch from './unsplashImageSearch';
-import { mockApiGetPhotosSuccessResponse, mockApiGetPhotosErrorResponse } from './__mocks__/unsplashImageSearch';
+import { mockApiGetPhotosSuccessResponse, mockApiGetPhotosErrorResponse, mockApiGetPhotosEmptyResponse } from './__mocks__/unsplashImageSearch';
 import { apiMethods } from '../utils/unsplashApi';
 import CONSTANT_STRINGS from '../strings/constantStrings';
 
-//spy on the apiGetPhotos function, replace it with a mock with default implementation
+//spy on the apiGetPhotos function, replace it with a mock with a default implementation that returns a promise that resolves to an example success response from the api
 const spiedApiGetPhotos = jest.spyOn(apiMethods, 'apiGetPhotos').mockImplementation(() => Promise.resolve(mockApiGetPhotosSuccessResponse));
 
 // Unmounts React trees that were mounted with render and clears screen variable that holds latest render output
@@ -14,8 +14,8 @@ afterEach(cleanup);
 beforeEach(() => spiedApiGetPhotos.mockClear());
 
 describe('UnsplashImageSearch Component effects', () => {
-  it('should make a call to the unsplash API on mount', async () => { 
-    await waitFor(() => render(<UnsplashImageSearch searchQuery={""} onImageDownload={null} width={"100"} height={"100"}></UnsplashImageSearch>));
+  it('should make a call to the unsplash API on mount given a non-blank searchQuery props', async () => { 
+    await waitFor(() => render(<UnsplashImageSearch searchQuery={"test query"} onImageDownload={null} width={"100"} height={"100"}></UnsplashImageSearch>));
     expect(spiedApiGetPhotos).toHaveBeenCalled();
   });
 
@@ -23,6 +23,12 @@ describe('UnsplashImageSearch Component effects', () => {
     let searchQueryProp = "mountains";
     await waitFor(() => render(<UnsplashImageSearch searchQuery={searchQueryProp} onImageDownload={null} width={"100"} height={"100"}></UnsplashImageSearch>));
     expect(spiedApiGetPhotos).toHaveBeenCalledWith(expect.objectContaining({query: searchQueryProp}));
+  });
+
+  it('should not make a call to the unsplash API on mount if the searchQuery provided in the props = ""', async () => { 
+    let searchQueryProp = "";
+    await waitFor(() => render(<UnsplashImageSearch searchQuery={searchQueryProp} onImageDownload={null} width={"100"} height={"100"}></UnsplashImageSearch>));
+    expect(spiedApiGetPhotos).not.toHaveBeenCalled();
   });
 
   it('should make a new call to the Unsplash API if the searchQuery prop is modified', async () => {
@@ -40,6 +46,10 @@ describe('UnsplashImageSearch Component effects', () => {
     //...and not with the original prop value
     expect(spiedApiGetPhotos).not.toHaveBeenCalledWith(expect.objectContaining({query: searchQueryProp}));
   })
+
+  it('should set the textInput value to empty string upon searchQuery prop change', async () => {
+    //TODO: implement
+  })
 })
 
 describe('search input text', () => {
@@ -48,7 +58,7 @@ describe('search input text', () => {
     await waitFor(() => expect(screen.getByPlaceholderText(CONSTANT_STRINGS.UNSPLASH_IMAGE_SEARCH_INPUT_PLACEHOLDER)).toBeTruthy());
   })
 
-  it('should hit the api with a new query upon submission of text', async () => {
+  it('should hit the api with a new query upon submission of non-blank text', async () => {
     render(<UnsplashImageSearch searchQuery={""} onImageDownload={null} width={"100"} height={"100"}></UnsplashImageSearch>);
     const textInput = await waitFor(() => screen.getByPlaceholderText(CONSTANT_STRINGS.UNSPLASH_IMAGE_SEARCH_INPUT_PLACEHOLDER));
     const newSearchQuery = "mountains";
@@ -57,17 +67,47 @@ describe('search input text', () => {
     await act(async() => fireEvent(textInput, 'submitEditing'));
     expect(spiedApiGetPhotos).toHaveBeenCalledWith(expect.objectContaining({query: newSearchQuery}));
   })
+
+  it('should not hit the api with a new query upon submission of blank text', async () => {
+    render(<UnsplashImageSearch searchQuery={"Query from props"} onImageDownload={null} width={"100"} height={"100"}></UnsplashImageSearch>);
+    const textInput = await waitFor(() => screen.getByPlaceholderText(CONSTANT_STRINGS.UNSPLASH_IMAGE_SEARCH_INPUT_PLACEHOLDER));
+    const newSearchQuery = "";
+    await act(async () => fireEvent.changeText(textInput, newSearchQuery));
+    expect(textInput.props.value).toBe(newSearchQuery);
+    await act(async() => fireEvent(textInput, 'submitEditing'));
+    expect(spiedApiGetPhotos).not.toHaveBeenCalledWith(expect.objectContaining({query: ""}));
+  })
+
+  it('should send the lowercase transform of the input to the api', async () => {
+    render(<UnsplashImageSearch searchQuery={""} onImageDownload={null} width={"100"} height={"100"}></UnsplashImageSearch>);
+    const textInput = await waitFor(() => screen.getByPlaceholderText(CONSTANT_STRINGS.UNSPLASH_IMAGE_SEARCH_INPUT_PLACEHOLDER));
+    const newSearchQuery = "MOuNTaINS aND OtherS";
+    await act(async () => fireEvent.changeText(textInput, newSearchQuery));
+    expect(textInput.props.value).toBe(newSearchQuery);
+    await act(async() => fireEvent(textInput, 'submitEditing'));
+    expect(spiedApiGetPhotos).toHaveBeenCalledWith(expect.objectContaining({query: "mountains and others"}));
+  })
+
+  it('should trim leading and trailing whitespace characters from the input before querying the api', async () => {
+    render(<UnsplashImageSearch searchQuery={""} onImageDownload={null} width={"100"} height={"100"}></UnsplashImageSearch>);
+    const textInput = await waitFor(() => screen.getByPlaceholderText(CONSTANT_STRINGS.UNSPLASH_IMAGE_SEARCH_INPUT_PLACEHOLDER));
+    const newSearchQuery = "\n  \r mountains  \n\r";
+    await act(async () => fireEvent.changeText(textInput, newSearchQuery));
+    expect(textInput.props.value).toBe(newSearchQuery);
+    await act(async() => fireEvent(textInput, 'submitEditing'));
+    expect(spiedApiGetPhotos).toHaveBeenCalledWith(expect.objectContaining({query: "mountains"}));
+  })
 })
 
 describe('activity indicator', () => {
-  it('should be shown initially', async () => {
-    render(<UnsplashImageSearch searchQuery={""}></UnsplashImageSearch>);
+  it('should be shown on initial render, given a non-blank searchQuery prop', async () => {
+    render(<UnsplashImageSearch searchQuery={"test query"}></UnsplashImageSearch>);
     await waitFor(() => expect(screen.getByRole('progressbar')).toBeTruthy());
   })
 
   it('should be hidden once the api call resolves', async () => {
     //render the component (render is wrapped in an act() call so no need to use here)
-    render(<UnsplashImageSearch searchQuery={""}></UnsplashImageSearch>);
+    render(<UnsplashImageSearch searchQuery={"test query"}></UnsplashImageSearch>);
     //get the result object value of the first call to the mocked apiGetPhotos function which should be a promise
     const resultOfApiCall = spiedApiGetPhotos.mock.results[0].value;
     //wrap in act because once the promise resolves the state is updated. Await the fulfillment of act before continuing to prevent unexpected behaviour
@@ -80,12 +120,29 @@ describe('activity indicator', () => {
   })
 })
 
+describe('on no results response from api', () => {
+  // it.only('should display the warning message', async () => {
+  //   //update the spiedApiGetPhotos mock to resolve to the unsplash empty response the next time it is called
+  //   spiedApiGetPhotos.mockResolvedValueOnce(mockApiGetPhotosEmptyResponse);
+  //   //render the component (render is wrapped in an act() call so no need to use here)
+  //   render(<UnsplashImageSearch searchQuery={"test query"}></UnsplashImageSearch>);
+  //   //get the result object value of the first call to the mocked apiGetPhotos function which should be a promise
+  //   const resultOfApiCall = spiedApiGetPhotos.mock.results[0].value;
+  //   //wrap in act because once the promise resolves the state is updated. Await the fulfillment of act before continuing to prevent unexpected behaviour
+  //   await act(async () => {
+  //     //wait for the result of the api call to be resolved and check the resolved value is the mocked response object
+  //     await expect(resultOfApiCall).resolves.toEqual(mockApiGetPhotosEmptyResponse);
+  //   });
+  //   expect(screen.getByText(CONSTANT_STRINGS.UNSPLASH_IMAGE_SEARCH_ON_NO_RESULTS_MESSAGE)).toBeDefined();
+  // })
+})
+
 describe('on error response from api', () => {
   it('should display the first error message from the errors object in the api response', async () => {
     //update the spiedApiGetPhotos mock to resolve to the unsplash error response the next time it is called
     spiedApiGetPhotos.mockResolvedValueOnce(mockApiGetPhotosErrorResponse);
     //render the component (render is wrapped in an act() call so no need to use here)
-    render(<UnsplashImageSearch searchQuery={""}></UnsplashImageSearch>);
+    render(<UnsplashImageSearch searchQuery={"test query"}></UnsplashImageSearch>);
     //get the result object value of the first call to the mocked apiGetPhotos function which should be a promise
     const resultOfApiCall = spiedApiGetPhotos.mock.results[0].value;
     //wrap in act because once the promise resolves the state is updated. Await the fulfillment of act before continuing to prevent unexpected behaviour
@@ -100,7 +157,7 @@ describe('on error response from api', () => {
     //update the spiedApiGetPhotos mock to resolve to the unsplash error response the next time it is called
     spiedApiGetPhotos.mockResolvedValueOnce(mockApiGetPhotosErrorResponse);
     //render the component (render is wrapped in an act() call so no need to use here)
-    render(<UnsplashImageSearch searchQuery={""}></UnsplashImageSearch>);
+    render(<UnsplashImageSearch searchQuery={"test query"}></UnsplashImageSearch>);
     //get the result object value of the first call to the mocked apiGetPhotos function which should be a promise
     const resultOfApiCall = spiedApiGetPhotos.mock.results[0].value;
     //wrap in act because once the promise resolves the state is updated. Await the fulfillment of act before continuing to prevent unexpected behaviour
