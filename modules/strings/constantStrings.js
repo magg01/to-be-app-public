efdfbb1223a81d4a1db23d1aace5d81ed3ba4514
Matthew <Magg01@gmail.com>
2022-08-11 21:02:53 +0200

    new static string for no results warning

--- modules/strings/constantStrings.js
+++ modules/strings/constantStrings.js
@@ -1,6 +1,7 @@
 const CONSTANT_STRINGS = {
   UNSPLASH_IMAGE_SEARCH_INPUT_PLACEHOLDER: "Search Unsplash for images",
   UNSPLASH_IMAGE_SEARCH_ON_ERROR_RESPONSE_MESSAGE: "We're sorry, there was an error getting images from Unsplash",
+  UNSPLASH_IMAGE_SEARCH_ON_NO_RESULTS_MESSAGE: "No matching photos could be found for your search. Please try a differnt search",
 }
 
 export default CONSTANT_STRINGS;
