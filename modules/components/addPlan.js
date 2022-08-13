ed0b796ac2a37fa82d64d4ce1ff8bcebf7db0ec0
Matthew <Magg01@gmail.com>
2022-08-13 13:33:17 +0200

    refactored add plan doesn't need database lookup

--- modules/components/addPlan.js
+++ modules/components/addPlan.js
@@ -1,20 +1,15 @@
 import React, { useEffect, useRef, useState } from 'react'
+import Animated from 'react-native-reanimated';
 import { StyleSheet, View, Text, TextInput, Alert, TouchableOpacity } from 'react-native'
 import * as db from '../database/database';
 import DateTimePicker from './dateTimePicker';
+import { animations } from '../utils/animations';
 
 export default AddPlan = (props) => {
   const toBeId = useRef(props.toBeId);
   const [newPlanTitle, setNewPlanTitle] = useState('');
-  const [toBeItem, setToBeItem] = useState(undefined);
   const [showDateTimePicker, setShowDateTimePicker] = useState(false);
   const calEvent = useRef(null);
-
-  useEffect(() => {
-    db.getToBeItemById(toBeId.current).then((toBeItem) => {
-      setToBeItem(toBeItem);
-    })
-  }, [toBeId.current])
 
   const addPlan = () => {
     db.addPlan(newPlanTitle, toBeId.current, calEvent.current)
@@ -40,31 +35,28 @@ export default AddPlan = (props) => {
     setShowDateTimePicker(false);
   }
 
-  if(toBeItem === undefined){
-    return(
-      <View style={styles.container}>
-        <Text>Loading from database</Text>
-      </View>  
-    )
-  } else {
-    return (
-      <View style={styles.container}>
-        <Text style={{color: 'white', fontSize: 20}}>How can you be more {toBeItem.title.toLowerCase()}?</Text>
-        <TextInput style={styles.input} onChangeText={(text) => setNewPlanTitle(text)} />
-        <TouchableOpacity style={styles.addButton} onPress={addPlan}>
-          <Text>Add</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.addButton} onPress={addCalendar}>
-          <Text>Cal</Text>
-        </TouchableOpacity>
-        {showDateTimePicker && <DateTimePicker 
-          calEvent={calEvent.current}
-          onDateTimeChange={(eventDate, eventStartTime, eventEndTime) => onDateTimeChange(eventDate, eventStartTime, eventEndTime)} 
-          onCancel={() => setShowDateTimePicker(false)}
-        />} 
-      </View>
-    )
-  }
+  return (
+    <Animated.View 
+      style={styles.container}
+      entering={animations.addPlan.addPlanView.entering}
+      exiting={animations.addPlan.addPlanView.exiting}
+    >
+
+      <Text style={{color: 'white', fontSize: 20}}>How can you be more {props.toBeItemTitle.toLowerCase()}?</Text>
+      <TextInput style={styles.input} onChangeText={(text) => setNewPlanTitle(text)} />
+      <TouchableOpacity style={styles.addButton} onPress={addPlan}>
+        <Text>Add</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.addButton} onPress={addCalendar}>
+        <Text>Cal</Text>
+      </TouchableOpacity>
+      {showDateTimePicker && <DateTimePicker 
+        calEvent={calEvent.current}
+        onDateTimeChange={(eventDate, eventStartTime, eventEndTime) => onDateTimeChange(eventDate, eventStartTime, eventEndTime)} 
+        onCancel={() => setShowDateTimePicker(false)}
+      />} 
+    </Animated.View>
+  )
 }
 
 const styles = StyleSheet.create({
