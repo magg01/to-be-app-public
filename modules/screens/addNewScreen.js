4767ee7b93db8e1720fd2bac11645ebe062cdeb4
Matthew <Magg01@gmail.com>
2022-08-14 21:13:08 +0200

    reset addnewscreen to inital state when blur/focus

--- modules/screens/addNewScreen.js
+++ modules/screens/addNewScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useCallback } from 'react';
 import { StyleSheet, View, ImageBackground, Text, TextInput, Button, Alert, Dimensions, TouchableOpacity } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { useHeaderHeight } from '@react-navigation/elements';
@@ -6,8 +6,9 @@ import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import UnsplashImageSearch from '../components/unsplashImageSearch';
 import { addToBeItem } from '../database/database';
+import { useFocusEffect } from '@react-navigation/native';
 
-const backgroundImage = require("../../assets/addNew.jpg");
+const defaultBackgroundImage = require("../../assets/addNew.jpg");
 const {height, width} = Dimensions.get('window');
 
 
@@ -16,9 +17,19 @@ const AddNewScreen = ({navigation}) => {
   const [searchQuery, setSearchQuery] = useState('');
   const [showSaveButton, setShowSaveButton] = useState(false);
   const [showImagePicker, setShowImagePicker] = useState(false);
-  const [imageBackgroundUri, setImageBackgroundUri] = useState(backgroundImage);
-  const insets = useSafeAreaInsets();
-  const headerHeight = useHeaderHeight();
+  const [imageBackgroundUri, setImageBackgroundUri] = useState(defaultBackgroundImage);
+
+  //reset the screen to initial state when focussed. When switching tabs this resets this screen
+  //when blurred and then refocussed
+  useFocusEffect(
+    useCallback(() => {
+      setShowSaveButton(false);
+      setShowImagePicker(false);
+      updateTitleText('');
+      setSearchQuery('');
+      setImageBackgroundUri(defaultBackgroundImage)
+    },[])
+  )
   
   const updateImageBackground = (uri) => {
     setShowImagePicker(false);
@@ -32,25 +43,25 @@ const AddNewScreen = ({navigation}) => {
   }
 
   return (
-      <ImageBackground source={imageBackgroundUri} resizeMode="cover" style={styles.container}>
-        <SafeAreaView style={styles.container}>
-          <TextInput
-            style={[styles.input, {marginTop: headerHeight - insets.top}]} 
-            onChangeText={updateTitleText}
-            onSubmitEditing={(e) => setSearchQuery(e.nativeEvent.text)}
-            value={titleText} 
-            textAlign={'center'}
-            autoFocus={true}
-            showSoftInputOnFocus={true}
-            selectionColor={'white'}
-          />
-          <Button title="show/hide" onPress={() => setShowImagePicker(!showImagePicker)} />
-          {showImagePicker ? <UnsplashImageSearch width={width*.75} height={height*.75} searchQuery={searchQuery} onImageDownload={updateImageBackground}></UnsplashImageSearch> : null}
-          <View style={{flex: 1}} />
-          {showSaveButton ? <TouchableOpacity style={{backgroundColor: '#ccc', margin: 12, padding: 12, borderRadius: 4}} onPress={onNewSave} ><Text>Save</Text></TouchableOpacity> : null}
-          <StatusBar style="auto" />
-        </SafeAreaView>
-      </ImageBackground>
+    <ImageBackground source={imageBackgroundUri} resizeMode="cover" style={styles.container}>
+      <SafeAreaView style={styles.container}>
+        <TextInput
+          style={styles.input} 
+          onChangeText={updateTitleText}
+          onSubmitEditing={(e) => setSearchQuery(e.nativeEvent.text)}
+          value={titleText} 
+          textAlign={'center'}
+          autoFocus={true}
+          showSoftInputOnFocus={true}
+          selectionColor={'white'}
+        />
+        <Button title="show/hide" onPress={() => setShowImagePicker(!showImagePicker)} />
+        {showImagePicker ? <UnsplashImageSearch width={width*.75} height={height*.75} searchQuery={searchQuery} onImageDownload={updateImageBackground}></UnsplashImageSearch> : null}
+        <View style={{flex: 1}} />
+        {showSaveButton ? <TouchableOpacity style={{backgroundColor: '#ccc', margin: 12, padding: 12, borderRadius: 4}} onPress={onNewSave} ><Text>Save</Text></TouchableOpacity> : null}
+        <StatusBar style="auto" />
+      </SafeAreaView>
+    </ImageBackground>
   )
 }
 
