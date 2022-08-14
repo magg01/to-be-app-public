cb32af515ea9572bf978f38bf8365531deb0243f
Matthew <Magg01@gmail.com>
2022-08-14 20:48:36 +0200

    icons added to tab bar

--- modules/screens/homeScreen.js
+++ modules/screens/homeScreen.js
@@ -1,4 +1,5 @@
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { SimpleLineIcons, Ionicons  } from '@expo/vector-icons'; 
 import { AgendaScreen } from './agendaScreen';
 import { AddNewScreen } from './addNewScreen';
 import { BeScreen } from './beScreen';
@@ -11,19 +12,41 @@ const HomeScreen = () => {
       initialRouteName='Be'
       activeColor="#f0edf6"
       inactiveColor="#3e2465"
-      barStyle={{ backgroundColor: '#694fad' }}
+      barStyle={{ backgroundColor: 'rgba(105, 79, 173, 1)' }}
     >
       <Tab.Screen 
         name="AddNew" 
         component={AddNewScreen} 
+        options={{
+          tabBarLabel: "New",
+          tabBarIcon: ({color}) => {
+            return (
+              <Ionicons name="add-circle-outline" size={24} color={color} />
+            )
+          }
+        }}
       />
       <Tab.Screen 
         name="Be" 
-        component={BeScreen} 
+        component={BeScreen}
+        options={{
+          tabBarIcon: ({color}) => {
+            return (
+              <Ionicons name="person-outline" size={24} color={color} />
+            )
+          }
+        }}
       />
       <Tab.Screen 
         name="Calendar" 
         component={AgendaScreen} 
+        options={{
+          tabBarIcon: ({color}) => {
+            return (
+              <SimpleLineIcons name="calendar" size={24} color={color} />
+            )
+          }
+        }}
       />
     </Tab.Navigator>
   )  
