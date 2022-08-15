f4b7a1d425092b0896dd3574979085a3b7258dca
Matthew <Magg01@gmail.com>
2022-08-15 17:09:56 +0200

    react native eslint rules added

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    'react-native/react-native': true, // whitelist all browser-like globals
   },
   extends: [
     'plugin:react/recommended',
@@ -16,10 +17,15 @@ module.exports = {
   plugins: [
     'react',
     'react-hooks',
+    'react-native',
   ],
   rules: {
     'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
     'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
     'linebreak-style': ['warn', 'windows'],
+    // previous rules here
+    'react-native/no-unused-styles': 2, // disallow unused styles
+    'react-native/no-inline-styles': 2, // disallow styles declared within the component itself
+    'react-native/no-color-literals': 2, // enforces variable names to be used for storing colors
   },
 };
