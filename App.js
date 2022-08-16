9f1a567b04341e9820ac7c1c48e6df7446cb602c
Matthew <Magg01@gmail.com>
2022-08-16 22:49:35 +0200

    switched tab to top level with inner stacks

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
 import { setNotificationHandler } from 'expo-notifications';
-import HomeScreen from './modules/screens/homeScreen';
+import { SimpleLineIcons, Ionicons } from '@expo/vector-icons';
 import ViewToBeScreen from './modules/screens/viewToBeScreen';
+import AgendaScreen from './modules/screens/agendaScreen';
 import { fetchPermissionSettings, checkPermissionSettings } from './modules/utils/notifications';
+import AddNewScreen from './modules/screens/addNewScreen';
+import BeScreen from './modules/screens/beScreen';
 
-const Stack = createStackNavigator();
+const Tab = createMaterialBottomTabNavigator();
+const BeStack = createStackNavigator();
+const AddNewStack = createStackNavigator();
 
 // Sets the handler function responsible for deciding
 // what to do with a notification that is received when the app is in foreground
@@ -33,20 +39,88 @@ setNotificationHandler({
   }
 })
 
+function BeStackScreen() {
+  return (
+    <BeStack.Navigator 
+      screenOptions={{
+        headerTintColor: 'white',
+        headerTransparent: true,
+        title: '',
+      }}
+    >
+      <BeStack.Screen name="BeScreen" component={BeScreen} />
+      <BeStack.Screen name="ViewToBeScreen" component={ViewToBeScreen} />
+    </BeStack.Navigator>
+  );
+}
+
+function AddNewStackScreen() {
+  return (
+    <AddNewStack.Navigator
+      screenOptions={{
+        headerTintColor: 'white',
+        headerTransparent: true,
+        title: '',
+      }}
+    >
+      <AddNewStack.Screen name="AddNewScreen" component={AddNewScreen} />
+    </AddNewStack.Navigator>
+  );
+}
+
 export default function App() {
   return (
+    // <NavigationContainer>
+    //   <Stack.Navigator
+    //     initialRouteName="HomeScreen"
+    //     screenOptions={{
+    //       headerTintColor: 'white',
+    //       headerTransparent: true,
+    //       title: ""
+    //     }}
+    //   >
+    //     <Stack.Screen name="HomeScreen" component={HomeScreen}/>
+    //     <Stack.Screen name="ViewToBeScreen" component={ViewToBeScreen} />
+    //   </Stack.Navigator>
+    // </NavigationContainer>
     <NavigationContainer>
-      <Stack.Navigator 
-        initialRouteName="HomeScreen" 
-        screenOptions={{
-          headerTintColor: 'white',
-          headerTransparent: true,
-          title: ""
-        }}
+      <Tab.Navigator
+        initialRouteName="Be"
+        activeColor="#f0edf6"
+        inactiveColor="#3e2465"
+        barStyle={{ backgroundColor: 'rgba(105, 79, 173, 1)' }}
+        backBehavior="initialRoute"
       >
-        <Stack.Screen name="HomeScreen" component={HomeScreen}/>
-        <Stack.Screen name="ViewToBeScreen" component={ViewToBeScreen} />
-      </Stack.Navigator>
+        <Tab.Screen
+          name="AddNew"
+          component={AddNewStackScreen}
+          options={{
+            tabBarLabel: 'New',
+            tabBarIcon: ({ color }) => (
+              <Ionicons name="add-circle-outline" size={24} color={color} />
+            ),
+            unmountOnBlur: true,
+          }}
+        />
+        <Tab.Screen
+          name="Be"
+          component={BeStackScreen}
+          options={{
+            tabBarIcon: ({color}) => (
+              <Ionicons name="person-outline" size={24} color={color} />
+            )
+          }}
+        />
+        <Tab.Screen
+          name="Calendar"
+          component={AgendaScreen}
+          options={{
+            tabBarIcon: ({color}) => (
+              <SimpleLineIcons name="calendar" size={24} color={color} />
+            )
+          }}
+        />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
