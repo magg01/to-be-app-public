e1d23731fea502682fc5cf804d7106ffbf2b6f29
Matthew <Magg01@gmail.com>
2022-08-18 20:52:56 +0200

    constant sting edit

--- modules/strings/constantStrings.js
+++ modules/strings/constantStrings.js
@@ -1,8 +1,11 @@
 const CONSTANT_STRINGS = {
   UNSPLASH_IMAGE_SEARCH: {
-    INPUT_PLACEHOLDER: "Refine your search",
+    INPUT_PLACEHOLDER: "Search for images",
     ON_ERROR_RESPONSE_MESSAGE: "We're sorry, there was an error getting images from Unsplash.",
     ON_NO_RESULTS_MESSAGE: "No matching photos could be found for your search. \n\nPlease try a different search.",
+  },
+  ADD_NEW_SCREEN: {
+    IMAGE_PICKER_INSTRUCTION: "Find a background image",
   },
 };
 
