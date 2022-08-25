4510cfa102a6ca332da8ab91eb90750b5bf1bc38
Matthew <Magg01@gmail.com>
2022-08-25 13:12:21 +0200

    eslint changes

--- modules/components/focusAwareStatusBar.js
+++ modules/components/focusAwareStatusBar.js
@@ -3,8 +3,8 @@ import { StatusBar } from 'expo-status-bar';
 import { useIsFocused } from '@react-navigation/native';
 
 // This component necessary for screens in Tab navigator (not stack navigator)
-// otherwise status bar shown is always that configured on the last tab screen in the navigator.
-// see docs https://reactnavigation.org/docs/status-bar/#tabs-and-drawer for more.
+// otherwise status bar shown on all screens is always that configured on the last tab
+// screen in the navigator. see docs https://reactnavigation.org/docs/status-bar/#tabs-and-drawer for more.
 function FocusAwareStatusBar({style}) {
   const isFocussed = useIsFocused();
   return isFocussed ? <StatusBar style={style} /> : null;
