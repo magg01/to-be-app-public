7216a3057003650cb0c13268ebcbea6347f54234
Matthew <Magg01@gmail.com>
2022-09-02 00:47:25 +0200

    tried to change jest setups to mock animation

--- jest-setup.js
+++ jest-setup.js
@@ -10,6 +10,6 @@ jest.mock('react-native-reanimated', () => {
   return Reanimated;
 });
 
-// Silence the warning: Animated: `useNativeDriver` is not supported 
+// Silence the warning: Animated: `useNativeDriver` is not supported
 // because the native animated module is missing
 jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
